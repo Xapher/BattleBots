cd90efcc96738ce767b2ac96677f7f23bda841a0
Tracie Wamsley <Tracie Wamsley@10.10.16.102>
2015-06-05 14:41:24 +0000

    whoops forgot some stoof

---
+++ src/tests/BattleBotTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dummyData.FakeMotor;
+
+public class BattleBotTests {
+
+	@Test
+	public void test() 
+	{
+		FakeMotor fm = new FakeMotor();
+		fm.forward();
+		fm.backwards();
+		fm.spin();
+		fm.stop();
+		fm.hammerSwing();
+		fm.setPower(12);
+	}
+
+}
